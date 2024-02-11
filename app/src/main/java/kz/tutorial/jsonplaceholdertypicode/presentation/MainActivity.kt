31097d9a4e3419a92a5bc9ed775ffa50031f77aa
frtank1 <frtank1@yande.ru>
2024-02-12 00:48:09 +0600

    first commit

--- app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/MainActivity.kt
+++ app/src/main/java/kz/tutorial/jsonplaceholdertypicode/presentation/MainActivity.kt
@@ -2,21 +2,28 @@ package kz.tutorial.jsonplaceholdertypicode.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import kz.tutorial.jsonplaceholdertypicode.R
+import kz.tutorial.jsonplaceholdertypicode.databinding.ActivityMainBinding
 import kz.tutorial.jsonplaceholdertypicode.presentation.posts.PostsFragment
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        initPostsFragment()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+               R.id.posts ->{Navigation.findNavController(binding.flContainer).navigate(R.id.posts)}
+                R.id.vector -> {}
+                R.id.users -> {}
+                R.id.profile -> {}
+            }
+            true
+        }
     }
 
-    private fun initPostsFragment() {
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fl_container, PostsFragment())
-            .commit()
-    }
+
 }
